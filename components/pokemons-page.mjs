4f951261b09a7ee3a600f888243960f1e23c8e02
dsvgit <dsv.mail@yandex.ru>
2024-11-03 19:46:34 +0500

    pokemon load more spinner

--- components/pokemons-page.mjs
+++ components/pokemons-page.mjs
@@ -3,21 +3,32 @@ import { Loading } from "./loading.mjs";
 import { PokemonCard } from "./pokemon-card.mjs";
 
 export const PokemonsPage = () => {
-  const next = signal("https://pokeapi.co/api/v2/pokemon?offset=0&limit=12");
+  const offset = 8;
+  const next = signal(
+    `https://pokeapi.co/api/v2/pokemon?offset=0&limit=${offset}`,
+  );
   const list = signal([]);
+  const loading = signal(false);
 
-  const fetchList = () =>
+  const fetchList = () => {
+    loading.value = true;
     fetch(next.value)
       .then((res) => res.json())
       .then((data) => {
         list.value = list.value.concat(data.results);
         next.value = data.next;
+        loading.value = false;
       });
+  };
 
   fetchList();
 
   const Column = ({ children }) =>
-    render`<div class="col col-6 col-md-4 col-lg-3 col-xl-2">${children}</div>`;
+    render`<div class="col col-6 col-md-3">${children}</div>`;
+
+  const spinner = $(
+    `<span class="spinner-border spinner-border-sm me-2" aria-hidden="true"></span>`,
+  );
 
   return render`
     <div>
@@ -32,18 +43,30 @@ export const PokemonsPage = () => {
       ${$(`<div class="row gx-3 gy-3"></div>`).useEffect((x) => {
         x.append(
           list.value
-            .slice(list.value.length - 12)
+            .slice(list.value.length - offset)
             .map((pokemon) => Column({ children: PokemonCard(pokemon) })),
         );
       })}
       
       <div class="d-flex justify-content-center p-4">
-        ${$(`<button class="btn btn-secondary btn-sm">Load more...</button>`).on(
-          "click",
-          () => {
+        ${$(`<button class="btn btn-secondary btn-sm">Load more...</button>`)
+          .on("click", () => {
             fetchList();
-          },
-        )}
+          })
+          .useEffect((x) => {
+            if (!list.value.length) {
+              x.hide();
+            } else {
+              x.show();
+            }
+          })
+          .useEffect((x) => {
+            if (loading.value) {
+              x.prepend(spinner);
+            } else {
+              spinner.remove();
+            }
+          })}
       </div>
     </div>
   `;
